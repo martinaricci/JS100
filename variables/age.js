ebfb1054a01012301846707ded6c756886a27828
Martina Ricci <martinaricci93@live.it>
2021-01-26 10:57:35 +1100

    my answers for 'variables' exercise

---
+++ variables/age.js
@@ -0,0 +1,8 @@
+let currentYear = 20;
+
+console.log(`You are ${currentYear} years old.`);
+console.log(`In 10 years, you will be ${currentYear + 10} years old.`);
+console.log(`In 20 years, you will be ${currentYear + 20} years old.`);
+console.log(`In 30 years, you will be ${currentYear + 30} years old.`)
+console.log(`In 40 years, you will be ${currentYear + 40} years old.`);
+
