7cca0e09ca0ad6283bf5f0c55809610ba532b166
Martina Ricci <martinaricci93@live.it>
2021-01-25 17:46:55 +1100

    deleted index.html and wrote my answers

---
+++ preparation/one.js
@@ -0,0 +1,22 @@
+// EX 3
+let foo = "bar";
+console.log(foo);
+foo;
+
+// EX 6
+// index
+// lazyDog
+
+// EX 7
+// index
+// lazyDog
+// operand2
+
+// EX 8
+// BIG_NUMBER
+// CatName
+
+// EX 9
+// quick_Fox
+// 1stCharacter
+// snake_case
